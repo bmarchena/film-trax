3ab59f74f9e7de86af237c551c64a806090384ec
noise <bryanmarchena@gmail.com>
2025-05-21 21:52:25 -0400

    Move generateDayCells to utils

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,5 +1,5 @@
 import { months, weekdays, type MonthAbbr } from "../constants/calendarConsts";
-import DayCell from "./DayCell";
+import { generateDayCells } from "../utils/calendarUtils";
 
 interface CalendarProps {
 	year: number;
@@ -19,34 +19,7 @@ const Calendar = ({ year, month }: CalendarProps) => {
 
 	const monthName = months[Object.keys(months)[month] as MonthAbbr];
 
-	const generateDayCells = () => {
-		const offset = date.getUTCDay();
-		let dayCells = [];
-
-		for (let i = 0; i < offset; i++) {
-			dayCells.push(<div className="border-1 m-0.25" key={`offset-${i}`} />);
-		}
-
-		const endOfMonth = date;
-		endOfMonth.setUTCMonth(month + 1);
-		endOfMonth.setDate(0);
-
-		const daysOfMonth = endOfMonth.getDate();
-
-		for (let i = 0; i < daysOfMonth; i++) {
-			dayCells.push(<DayCell day={i} key={`${month}-${i}`} />);
-		}
-
-		const padding = 6 - endOfMonth.getDay();
-
-		for (let i = 0; i < padding; i++) {
-			dayCells.push(<div className="border-1 m-0.25" key={`padding-${i}`} />);
-		}
-
-		return dayCells;
-	};
-
-	const dayCells = generateDayCells();
+	const dayCells = generateDayCells(date, month);
 
 	return (
 		<div className="grid grid-cols-7 border-1">
