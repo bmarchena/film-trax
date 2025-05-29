525c19e5ff3b9ffa88624a0366a3e41c83959e7f
noise <bryanmarchena@gmail.com>
2025-05-28 22:57:58 -0400

    Add selected day logic and buttons to change year

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -10,6 +10,9 @@ const Calendar = () => {
 
 	const [date, setDate] = useState(today);
 
+	const [selectedDay, setSelectedDay] = useState(today);
+	console.log("selected", selectedDay);
+
 	const monthName = monthsArr[date[0] - 1];
 
 	const weekdayColumns = Object.keys(weekdays).map((weekday) => {
@@ -20,7 +23,7 @@ const Calendar = () => {
 		);
 	});
 
-	function updateMonth(increment: boolean) {
+	function incrementMonth(increment: boolean) {
 		const newDate = [...date];
 
 		if (increment) {
@@ -40,10 +43,17 @@ const Calendar = () => {
 		setDate([...newDate]);
 	}
 
-	function updateYear(increment: boolean) {
+	function incrementYear(increment: boolean) {
 		const newDate = [...date];
 		if (increment) newDate[2] = newDate[2] + 1;
 		else newDate[2] = newDate[2] - 1;
+		setDate([...newDate]);
+	}
+
+	function selectDay(day: number) {
+		const newDate = [...date];
+		newDate[1] = day;
+		setSelectedDay([...newDate]);
 	}
 
 	function generateDayCells() {
@@ -60,7 +70,15 @@ const Calendar = () => {
 		const daysInMonth = endOfMonth.getDate();
 
 		for (let i = 0; i < daysInMonth; i++) {
-			dayCells.push(<DayCell day={i} key={`${endOfMonth.getMonth()}-${i}`} />);
+			const selected = selectedDay[1] - 1 === i && selectedDay[0] === date[0];
+			dayCells.push(
+				<DayCell
+					day={i}
+					selected={selected}
+					selectDay={selectDay}
+					key={`${endOfMonth.getMonth()}-${i}`}
+				/>
+			);
 		}
 
 		const padding = 6 - endOfMonth.getDay();
@@ -76,23 +94,33 @@ const Calendar = () => {
 		<>
 			<div className="grid grid-cols-7 border-1">
 				<div className="border-1 grid grid-cols-3 col-span-full m-0.25 p-5">
-					<img
-						src="/src/assets/left-arrow.png"
-						className="justify-self-start self-center max-w-10 hover:cursor-pointer hover:text-amber-300"
-						onClick={() => {
-							updateMonth(false);
-						}}
-					/>
+					<div>
+						<img
+							src="/src/assets/arrow-left.png"
+							className="justify-self-start self-center max-w-10 hover:cursor-pointer hover:text-amber-300"
+							onClick={() => incrementMonth(false)}
+						/>
+						<img
+							src="/src/assets/double-arrow-left.png"
+							className="justify-self-start self-center max-w-9 hover:cursor-pointer hover:text-amber-300"
+							onClick={() => incrementYear(false)}
+						/>
+					</div>
 					<div className="text-xl">
 						{monthName} <br /> {date[2]}
 					</div>
-					<img
-						src="/src/assets/right-arrow.png"
-						className="justify-self-end self-center max-w-10 hover:cursor-pointer hover:text-amber-300"
-						onClick={() => {
-							updateMonth(true);
-						}}
-					/>
+					<div>
+						<img
+							src="/src/assets/arrow-right.png"
+							className="justify-self-end self-center max-w-10 hover:cursor-pointer hover:text-amber-300"
+							onClick={() => incrementMonth(true)}
+						/>
+						<img
+							src="/src/assets/double-arrow-right.png"
+							className="justify-self-end self-center max-w-9 hover:cursor-pointer hover:text-amber-300"
+							onClick={() => incrementYear(true)}
+						/>
+					</div>
 				</div>
 				{weekdayColumns}
 				{generateDayCells()}
--- src/components/DayCell.tsx
+++ src/components/DayCell.tsx
@@ -1,15 +1,24 @@
+import { useState } from "react";
+
 interface DayCellProps {
 	day: number;
+	selected?: boolean;
+	selectDay: (day: number) => void;
 }
 
-const DayCell = ({ day }: DayCellProps) => {
+const DayCell = ({ day, selected, selectDay }: DayCellProps) => {
 	const clickHandler = () => {
+		selectDay(day + 1);
 		console.log(`Clicked on day ${day + 1}`);
 	};
 
 	return (
 		<div
-			className="border-1 m-0.25 hover:cursor-pointer hover:text-amber-300"
+			className={
+				selected
+					? "border-1 m-0.25 hover:cursor-pointer text-blue-300"
+					: "border-1 m-0.25 hover:cursor-pointer hover:text-amber-300"
+			}
 			onClick={clickHandler}
 		>
 			<div className="border-1 p-1">{day + 1}</div>
